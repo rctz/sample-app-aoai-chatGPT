2b7ba9e8204a9df95c2ad26347ac3b912bab6a64
Rachata Singchinda <46108793+rctz@users.noreply.github.com>
2024-07-30 23:42:51 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -27,7 +27,7 @@ cd ..
 echo ""
 echo "Starting backend"
 echo ""
-gunicorn --bind=0.0.0.0 --timeout 600 app:app
+./.venv/bin/python -m quart run --port=50505 --host=127.0.0.1 --reload
 if [ $? -ne 0 ]; then
     echo "Failed to start backend"
     exit $?
